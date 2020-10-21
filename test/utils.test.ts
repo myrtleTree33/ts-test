33eeb533aa95f8bdb5773b75c90f49bc579a7478
jhtong <me@joeltong.org>
2020-10-21 11:54:03 +0800

    Added tests
    - Increased line length
    - Added sample test

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import { sum } from '../src/Utils';
+import { sum, sumN } from '../src/Utils';
 
 describe('utils', () => {
   describe('#sum()', () => {
@@ -13,6 +13,38 @@ describe('utils', () => {
 
       it('should return a number', () => expect(actual).toEqual(expect.any(Number)));
       it('should add the numbers correctly', () => expect(actual).toEqual(5));
+    });
+  });
+
+  describe('#sumN()', () => {
+    describe('when given 1 positive number', () => {
+      let actual: number;
+
+      beforeAll(() => {
+        actual = sumN(1);
+      });
+
+      it('returns the number', () => expect(actual).toBe(1));
+    });
+
+    describe('when given 3 positive numbers', () => {
+      let actual: number;
+
+      beforeAll(() => {
+        actual = sumN(1, 2, 3);
+      });
+
+      it('sums the numbers correctly', () => expect(actual).toBe(6));
+    });
+
+    describe('when given 9 poositive numbers', () => {
+      let actual: number;
+
+      beforeAll(() => {
+        actual = sumN(1, 2, 1, 1, 2, 1, 1, 2, 1);
+      });
+
+      it('sums the numbers correctly', () => expect(actual).toBe(12));
     });
   });
 });
